4f63a4961af22258e1f098f18ebd67ae78b2ada8
Amin <aminallah.bakhshaei@gmail.com>
2024-05-18 14:11:21 +0330

    Init project.

---
+++ SpaceXLaunches/SpaceXLaunchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXLaunchesApp.swift
+//  SpaceXLaunches
+//
+//  Created by . on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXLaunchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
